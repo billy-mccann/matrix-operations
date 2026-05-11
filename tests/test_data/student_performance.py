66caa31848ac0e6f8fe7000df6a80a0b7ac96179
Bill McCann <hyperfine262@gmail.com>
2026-05-11 10:01:04 -0700

    Conform to type

--- tests/test_data/student_performance.py
+++ tests/test_data/student_performance.py
@@ -4,7 +4,6 @@ from src.linear_regression import LinearRegression
 
 path = "/Users/bill/.cache/kagglehub/datasets/nikhil7280/student-performance-multiple-linear-regression/versions/1/Student_Performance.csv"
 
-
 print("Path to dataset files:", path)
 
 with open(path) as file:
@@ -14,22 +13,16 @@ with open(path) as file:
     matrix = [row for row in reader]
     print(len(matrix))
 
-    max_row = 2000
-    # for i in range(max_row):
-    #     print(matrix[i])
+    max_row = 7000
 
     smaller = matrix[:max_row]
 
     for row in smaller:
-        row[2] = 1.0 if row[2] == 'Yes' else 0.0
+        row[2] = '1.0' if row[2] == 'Yes' else '0.0'
 
-    smaller = [[float(entry) for entry in row] for row in smaller]
+    training_matrix= [[float(entry) for entry in row] for row in smaller]
 
-    # print("==============") 
-    # for row in smaller:
-    #     print(row)
-
-    model = LinearRegression.train_least_squares(smaller)
+    model = LinearRegression.train_least_squares(training_matrix)
 
     test = [[7.0, 99.0, 1.0, 9.0, 1.0],
             [4.0, 82.0, 0.0, 4.0, 2.0]]
